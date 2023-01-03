0a073d32c72b4637e38dc8e0979173a86387c03b
pooooh14 <monisha1497@gmail.com>
2023-01-04 02:42:01 +0530

    add check prime no

---
+++ prime.js
@@ -0,0 +1,15 @@
+let num = 43;
+
+    for(i = 1; i <= num; i++) {
+        if(num % i == 0) {
+            
+            count++;
+            
+        }
+    }
+    if(count == 2) {
+        console.log("Yes");
+    }
+    else {
+        console.log("No");
+    }
